2705b1f7009e58d16a9e243a0dfda548da5d02e8
Raymond Perminov <raymondproduction@ukr.net>
2017-03-30 09:58:57 +0300

    Added function fetchPhotos

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -18,7 +18,12 @@ FlickrFetcher = {
   fetchFlickrData: function(apiKey, fetch) {
     var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&text=pugs&format=json&nojsoncallback=1'
     return fetch(url);
-  }
+  },
+  fetchPhotos: function(apiKey, fetch) {
+    return FlickrFetcher.fetchFlickrData(apiKey, fetch).then(function(data) {
+      return data.photos.photo.map(FlickrFetcher.transformPhotoObj);
+    });
+  },
 };
 
 module.exports = FlickrFetcher;
