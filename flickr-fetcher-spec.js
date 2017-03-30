e543bd97b0b4e2db7738ce6d04622f878fed00a9
Raymond Perminov <raymondproduction@ukr.net>
2017-03-30 09:54:40 +0300

    Working with network requests in TDD

--- flickr-fetcher-spec.js
+++ flickr-fetcher-spec.js
@@ -4,8 +4,7 @@ var FlickrFetcher = require('./flickr-fetcher.js');
 var expect = require('chai').expect;
 
 describe('#photoObjToURL()', function() {
-    it('should take a photo object from Flickr and return a string',
-        function() {
+    it('should take a photo object from Flickr and return a string', function() {
             var input = {
                 id: '24770505034',
                 owner: '97248275@N03',
@@ -38,7 +37,7 @@ describe('#photoObjToURL()', function() {
         });
 });
 describe('#transformPhotoObj()', function() {
-    it('take a photo object and return an object with just title and URL',
+    it('should take a photo object and return an object with just title and URL',
         function() {
             var input = {
                 id: '25373736106',
@@ -55,7 +54,7 @@ describe('#transformPhotoObj()', function() {
                 title: 'Dog goes to desperate measure to avoid walking on a leash',
                 url: 'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg',
             };
-            actual = FlickrFetcher.transformPhotoObj(input);
+            var actual = FlickrFetcher.transformPhotoObj(input);
             expect(actual).to.eql(expected);
 
             input = {
@@ -76,4 +75,53 @@ describe('#transformPhotoObj()', function() {
             actual = FlickrFetcher.transformPhotoObj(input);
             expect(actual).to.eql(expected);
         });
+});
+
+// flickr-fetcher-spec.js
+describe('#fetchFlickrData()', function() {
+    it(
+        'should take an API key and fetcher function argument and return a promise for JSON data.',
+        function() {
+            var apiKey      = 'does not matter much what this is right now',
+                fakeData    = {
+                    'photos': {
+                        'page':    1,
+                        'pages':   2872,
+                        'perpage': 100,
+                        'total':   '287170',
+                        'photo':   [{
+                            'id':       '24770505034',
+                            'owner':    '97248275@N03',
+                            'secret':   '31a9986429',
+                            'server':   '1577',
+                            'farm':     2,
+                            'title':    '20160229090898',
+                            'ispublic': 1,
+                            'isfriend': 0,
+                            'isfamily': 0
+                        }, {
+                            'id':       '24770504484',
+                            'owner':    '97248275@N03',
+                            'secret':   '69dd90d5dd',
+                            'server':   '1451',
+                            'farm':     2,
+                            'title':    '20160229090903',
+                            'ispublic': 1,
+                            'isfriend': 0,
+                            'isfamily': 0
+                        }]
+                    }
+                },
+                fakeFetcher = function(url) {
+                    var expectedURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+                                + apiKey + '&text=pugs&format=json&nojsoncallback=1'
+                    expect(url).to.equal(expectedURL)
+                    return Promise.resolve(fakeData);
+                };
+            return FlickrFetcher.fetchFlickrData(apiKey, fakeFetcher).then(function(actual) {
+                expect(actual).to.eql(fakeData);
+            }
+        );
+
+    });
 });
